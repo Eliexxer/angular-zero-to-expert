a88b690792f099bfb60809d051d5617c6139add3
Eliexer <eliexer1129@gmail.com>
2026-06-21 02:51:49 -0400

    feat: initialize repository with TypeScript bases and project workspace

---
+++ Bases de TypeScript/src/topics/01-basic-types.ts
@@ -0,0 +1,10 @@
+let name = 'Strider';
+let hpPoints: number | string = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL';
+hpPoints = 50;
+
+// console.log(name, hpPoints, isAlive);
+
+export {};
